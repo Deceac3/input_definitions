d6feac649c60fd492f3e989c83ffd21f65356896
Decease <Romab1cool1@mail.ru>
2024-06-06 01:14:50 +0700

    Создаём первую функцию: ввод путя напрямую через консоль и возвращаем строку с первоначальным путём

--- operations.h
+++ operations.h
@@ -5,4 +5,11 @@
 #define MAX_PATH 260
 #define DEEP_WAY 5
 
+char* line_input();
 
+char* line_input(){
+    char *giga;
+    giga = (char*)malloc(MAX_PATH*sizeof(int));
+    fgets(giga,MAX_PATH,stdin);
+    return (giga);
+}
