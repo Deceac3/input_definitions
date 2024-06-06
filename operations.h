79ce82ec04ec06d77d5f5da84ba78a5d52d7ff9a
Decease <Romab1cool1@mail.ru>
2024-06-07 03:04:34 +0700

    Закончили полностью проверку записи пути) Осталось дописать запись через айпишники и доделать мульти запись адресов через +

--- operations.h
+++ operations.h
@@ -24,7 +24,7 @@ void path_wiev(char*);
 _Bool processing(struct new_path*,char*);
 
 _Bool check_c(int, char*);
-
+_Bool path_control(struct new_path*,size_t*,char*);
 
 char* line_input(){
     char *giga;
@@ -64,7 +64,7 @@ _Bool check(char* inp){
 }
 
 void path_wiev(char* buf){
-    printf("new path: %s", buf);
+    printf("new path: %s\n", buf);
 }
 
 _Bool processing(struct new_path* path,char* buf){
@@ -86,8 +86,38 @@ _Bool processing(struct new_path* path,char* buf){
                         path->path[path->count]='/';
                         path->count++;    // На этом моменте мы имеем 1- наименование католога 2-всё остальное пути в катологе
                         i++;
-                        printf("Всё хорошо, записали путь еее\n");
-                        return true;
+                        _Bool ccheck = true;
+                        int count = 0;
+                        while (ccheck)
+                        {
+                            if (count <=4){
+                                if(path_control(path,&i,buf)){
+                                    if (buf[i]=='+')
+                                    {
+                                        printf("путь записан");
+                                        ccheck = false;
+                                        i++;
+                                    }
+                                    else if (buf[i]=='\n')
+                                    {
+                                        return true;
+                                    }
+                                    else{
+                                        count++;
+                                        i++;
+                                    }
+                                }
+                                else
+                                {
+                                    return false;
+                                }
+                            }
+                            else
+                            {
+                                printf("Error: длинна одного стандартного пути не может превышать 5 эллементов\n");
+                                return false;
+                            }
+                        }
                     }
                     else
                     {
@@ -134,5 +164,47 @@ _Bool check_c(int i,char* buf){
     else
     {
         return false;
+    }
+}
+
+_Bool path_control(struct new_path *path, size_t* i, char *buf){
+    size_t dot_count = 0;
+    while (true)
+    {
+        if (((buf[*i]>='A')&&(buf[*i]<='Z'))||((buf[*i]>='a')&&(buf[*i]<='z'))||((buf[*i]>='0')&&buf[*i]<='9'))
+        {
+            path->path[path->count]=buf[*i];
+            path->count++;
+            *i=*i+1;
+        }
+        else if (buf[*i]=='.')
+        {
+            if (dot_count<1)
+            {
+                path->path[path->count]=buf[*i];
+                path->count++;
+                dot_count++;
+                *i=*i+1;
+            }
+            else
+            {
+                printf("Error: в записи путя не может быть более 1 точки\n");
+                return false;
+            }
+        }
+        else if (buf[*i]=='\\')
+        {
+            path->path[path->count]='/';
+            path->count++;
+            return true;
+        }
+        else if (buf[*i]=='+')
+        {
+            return true;
+        }
+        else if (buf[*i]=='\n')
+        {
+            return true;
+        }
     }
 }
